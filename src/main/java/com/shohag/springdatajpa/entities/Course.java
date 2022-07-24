864019152656024f181b8483d9db4cd5466d7a88
sohag-aust <frustratedcoder96@gmail.com>
2022-07-25 01:36:41 +0600

    @ManyToMany relationship between student and course

--- src/main/java/com/shohag/springdatajpa/entities/Course.java
+++ src/main/java/com/shohag/springdatajpa/entities/Course.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -32,4 +34,27 @@ public class Course {
             mappedBy = "course"
     )
     private CourseMaterial courseMaterial;
+
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+         name = "student_course_map",
+            joinColumns = @JoinColumn(
+                    name = "course_id",
+                    referencedColumnName = "courseId"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId"
+            )
+    )
+    private List<Student> students;
+
+    public void addStudents(Student student) {
+        if(students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(student);
+    }
 }
--- src/test/java/com/shohag/springdatajpa/repositories/CourseRepositoryTest.java
+++ src/test/java/com/shohag/springdatajpa/repositories/CourseRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.shohag.springdatajpa.repositories;
 
 import com.shohag.springdatajpa.entities.Course;
+import com.shohag.springdatajpa.entities.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,5 +18,22 @@ class CourseRepositoryTest {
     public void getAllCourses() {
         List<Course> courses = courseRepository.findAll();
         System.out.println(courses);
+    }
+
+    @Test
+    public void saveCourseWithStudent() {
+        Student student = Student.builder()
+                .firstName("Mehedi")
+                .lastName("Ashik")
+                .emailId("ashik@gmail.com")
+                .build();
+
+        Course course = Course.builder()
+                .title("Artificial Intelligence")
+                .credit(7)
+                .build();
+
+        course.addStudents(student);
+        courseRepository.save(course);
     }
 }
